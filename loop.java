5837865d775d68f7d973fee9a0f76a844d99a10e
jenith varma <89399356+jenithvalar@users.noreply.github.com>
2023-11-29 15:05:38 +0530

    Update loop.java

--- loop.java
+++ loop.java
@@ -1,6 +1,6 @@
 public class SimpleLoop {
     public static void main(String[] args) {
-        for (int i = 1; i <= 20; i++) {
+        for (int i = 1; i <= 5; i++) {
             System.out.println(i);
         }
     }
